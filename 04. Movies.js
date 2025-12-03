3be8628e3fccc5a98545f2a1373176729e4249ed
Boyan Venkov <boyanvenkov@yahoo.com>
2025-12-03 11:27:01 +0200

    Added a New Project

---
+++ 04. Movies.js
@@ -0,0 +1,30 @@
+function movies(inputArray) {
+    let allMovies = [];
+
+    for (const cmdArg of inputArray) {
+        if (cmdArg.startsWith('addMovie ')) {
+            let movieName = cmdArg.substring(9);
+            allMovies.push({ name: movieName });
+        }
+        else if (cmdArg.includes("directedBy")) {
+            let [movieName, director] = cmdArg.split(" directedBy ");
+            let movie = allMovies.find(m => m.name === movieName);
+            if (movie) {
+                movie.director = director;
+            }
+        }
+        else if (cmdArg.includes("onDate")) {
+            let [movieName, movieDate] = cmdArg.split(" onDate ");
+            let movie = allMovies.find(m => m.name === movieName);
+            if (movie) {
+                movie.date = movieDate;
+            }
+        }
+    }
+
+    for (const movie of allMovies) {
+        if (movie.name && movie.director && movie.date) {
+            console.log(JSON.stringify(movie));
+        }
+    }
+}
